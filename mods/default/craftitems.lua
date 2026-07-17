6d2db661e1648ae9e7741e8c6107f288e36962c6
YCaTGameRT <ycatgamert@gmail.com>
2026-07-18 03:38:18 +0500

    renamed default in craftitems.lua

--- mods/default/craftitems.lua
+++ mods/default/craftitems.lua
@@ -4,103 +4,103 @@
 -- Crafting items
 --
 
-minetest.register_craftitem("default:stick", {
+minetest.register_craftitem("mcre_default:stick", {
 	description = "Stick",
 	inventory_image = "default_stick.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:paper", {
+minetest.register_craftitem("mcre_default:paper", {
 	description = "Paper",
 	inventory_image = "default_paper.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:book", {
+minetest.register_craftitem("mcre_default:book", {
 	description = "Book",
 	inventory_image = "default_book.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:coal_lump", {
+minetest.register_craftitem("mcre_default:coal_lump", {
 	description = "Coal Lump",
 	inventory_image = "default_coal_lump.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:charcoal_lump", {
+minetest.register_craftitem("mcre_default:charcoal_lump", {
 	description = "Charcoal Lump",
 	inventory_image = "default_charcoal_lump.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:gold_nugget", {
+minetest.register_craftitem("mcre_default:gold_nugget", {
 	description = "Gold Nugget",
 	inventory_image = "default_gold_nugget.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:diamond", {
+minetest.register_craftitem("mcre_default:diamond", {
 	description = "Diamond",
 	inventory_image = "default_diamond.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:clay_lump", {
+minetest.register_craftitem("mcre_default:clay_lump", {
 	description = "Clay Lump",
 	inventory_image = "default_clay_lump.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:steel_ingot", {
+minetest.register_craftitem("mcre_default:steel_ingot", {
 	description = "Steel Ingot",
 	inventory_image = "default_steel_ingot.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:gold_ingot", {
+minetest.register_craftitem("mcre_default:gold_ingot", {
 	description = "Gold Ingot",
 	inventory_image = "default_gold_ingot.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:emerald", {
+minetest.register_craftitem("mcre_default:emerald", {
 	description = "Emerald",
 	inventory_image = "default_emerald.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:clay_brick", {
+minetest.register_craftitem("mcre_default:clay_brick", {
 	description = "Clay Brick",
 	inventory_image = "default_clay_brick.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:flint", {
+minetest.register_craftitem("mcre_default:flint", {
 	description = "Flint",
 	inventory_image = "default_flint.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:gunpowder", {
+minetest.register_craftitem("mcre_default:gunpowder", {
 	description = "Gunpowder",
 	inventory_image = "default_gunpowder.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:bone", {
+minetest.register_craftitem("mcre_default:bone", {
 	description = "Bone",
 	inventory_image = "default_bone.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:glowstone_dust", {
+minetest.register_craftitem("mcre_default:glowstone_dust", {
 	description = "Glowstone Dust",
 	inventory_image = "default_glowstone_dust.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:fish_raw", {
+minetest.register_craftitem("mcre_default:fish_raw", {
 	description = "Raw Fish",
     groups = {},
     inventory_image = "default_fish.png",
@@ -108,7 +108,7 @@ minetest.register_craftitem("default:fish_raw", {
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:fish", {
+minetest.register_craftitem("mcre_default:fish", {
 	description = "Cooked Fish",
     groups = {},
     inventory_image = "default_fish_cooked.png",
@@ -116,49 +116,49 @@ minetest.register_craftitem("default:fish", {
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:sugar", {
+minetest.register_craftitem("mcre_default:sugar", {
 	description = "Sugar",
 	inventory_image = "default_sugar.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:string",{
+minetest.register_craftitem("mcre_default:string",{
 	description = "String",
 	inventory_image = "default_string.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:prismarine_cry", {
+minetest.register_craftitem("mcre_default:prismarine_cry", {
 	description = "Prismarine Crystals",
 	inventory_image = "default_prismarine_crystals.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:prismarine_shard", {
+minetest.register_craftitem("mcre_default:prismarine_shard", {
 	description = "Prismarine Shard",
 	inventory_image = "default_prismarine_shard.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:quartz_crystal", {
+minetest.register_craftitem("mcre_default:quartz_crystal", {
 	description = "Quartz Crystal",
 	inventory_image = "default_quartz_crystal.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:lapis_lazuli", {
+minetest.register_craftitem("mcre_default:lapis_lazuli", {
 	description = "Lapis Lazuli",
 	inventory_image = "default_lapis.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:bone_meal", {
+minetest.register_craftitem("mcre_default:bone_meal", {
 	description = "Bone Meal",
 	inventory_image = "default_bone_meal.png",
 	stack_max = 64,
 })
 
-minetest.register_craftitem("default:ink_sac", {
+minetest.register_craftitem("mcre_default:ink_sac", {
 	description = "Ink Sac",
 	inventory_image = "default_ink_sac.png",
 	stack_max = 64,
