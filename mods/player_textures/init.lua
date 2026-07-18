60cf91811d9da930acea0e3242ec816b9b40e0a8
YCaTGameRT <ycatgamert@gmail.com>
2026-07-19 00:28:23 +0500

    renamed player_texure again

--- mods/player_textures/init.lua
+++ mods/player_textures/init.lua
@@ -1,5 +1,5 @@
 minetest.register_on_joinplayer(function(player)
-	local filename = minetest.get_modpath("player_textures").."/textures/player_"..player:get_player_name()
+	local filename = minetest.get_modpath("mcre_player_textures").."/textures/player_"..player:get_player_name()
 	local f = io.open(filename..".png")
 	if f then
 		f:close()
