ed2582bf42cf7ef5089de9e54e216554a8afdb65
YCaTGameRT <ycatgamert@gmail.com>
2026-07-18 18:38:18 +0500

    renamed inventory

--- mods/inventory/workbench.lua
+++ mods/inventory/workbench.lua
@@ -1,4 +1,4 @@
-minetest.register_node("inventory:crafting_table", {
+minetest.register_node("mcre_inventory:crafting_table", {
 	description = "Crafting Table",
 	tiles = {"inventory_crafting_table_top.png", "default_wood.png", "inventory_crafting_table_side.png",
 		"inventory_crafting_table_side.png", "inventory_crafting_table_front.png", "inventory_crafting_table_front.png"},
@@ -7,7 +7,7 @@ minetest.register_node("inventory:crafting_table", {
 	groups = {choppy=2,oddly_breakable_by_hand=1,flammable=2},
 	sounds = default.node_sound_wood_defaults(),
 	on_rightclick = function(pos, node, clicker, itemstack)
-		minetest.show_formspec(clicker:get_player_name(), "inventory:craftin_table", CRAFTING_FORMSPEC)
+		minetest.show_formspec(clicker:get_player_name(), "mcre_inventory:craftin_table", CRAFTING_FORMSPEC)
 	end,
 })
 
